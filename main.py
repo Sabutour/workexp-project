c536eb73c11e7a8fd8944b0e931bcbe46627f477
Jordan McDonald <jmacthetaii@gmail.com>
2018-05-01 16:51:52 +1000

    UPDATE - END OF DAY

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ import textwrap
 import urllib
 
 from google.appengine.ext import ndb
+from google.appengine.api import users
+
+user = users.get_current_user()
+if user:
+    nickname = user.nickname()
+    logout_url = users.create_logout_url('/')
+    greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+        nickname, logout_url)
+else:
+    login_url = users.create_login_url('/login')
+    greeting = '<a href="{}">Sign in</a>'.format(login_url)
 
 # class Account(ndb.Model):
 #     """Account details."""
