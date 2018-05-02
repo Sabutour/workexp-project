68b19e2390f7e6045b6b37801359a3ba0cdf1596
Jordan McDonald <Jmacthetaii@gmail.com>
2018-05-02 20:46:35 +1000

    js Scripts
    Changes:
    - Clock script from 'index.html' moved to 'clock.js'
    - Changing Background script moved from 'index.html' to 'background_images.js'

---
+++ js/background_images.js
@@ -0,0 +1,36 @@
+function changeBackgroundPurple() {
+  document.getElementById("main_container").style.backgroundImage = "url('/static/images/background-pflower.jpg')";
+  document.getElementById("event_popup_header").style.backgroundColor = "#B648FD";
+  document.getElementById("note_popup_header").style.backgroundColor = "#B648FD";
+}
+  
+function changeBackgroundBlue() {
+  document.getElementById("main_container").style.backgroundImage = "url('/static/images/background-tree.jpeg')";
+  document.getElementById("event_popup_header").style.backgroundColor = "#56a0d9";
+  document.getElementById("note_popup_header").style.backgroundColor = "#56a0d9";
+}
+  
+function changeBackgroundRed() {
+  document.getElementById("main_container").style.backgroundImage = "url('/static/images/background-rflower.jpeg')";
+  document.getElementById("event_popup_header").style.backgroundColor = "#BB0303";
+  document.getElementById("note_popup_header").style.backgroundColor = "#BB0303";
+}
+  
+function changeBackgroundYellow() {
+  document.getElementById("main_container").style.backgroundImage = "url('/static/images/background-yflower.jpg')";
+  document.getElementById("event_popup_header").style.backgroundColor = "#FABB02";
+  document.getElementById("note_popup_header").style.backgroundColor = "#FABB02";
+}
+  
+function changeBackgroundOrange() {
+  document.getElementById("main_container").style.backgroundImage = "url('/static/images/background-web.jpg')";
+  document.getElementById("event_popup_header").style.backgroundColor = "#FF8300";
+  document.getElementById("note_popup_header").style.backgroundColor = "#FF8300";
+}
+  
+function changeBackgroundGreen() {
+  document.getElementById("main_container").style.backgroundImage = "url('/static/images/background-grass.jpg')";
+  document.getElementById("event_popup_header").style.backgroundColor = "#33AC20";
+  document.getElementById("note_popup_header").style.backgroundColor = "#33AC20";
+}
+  
